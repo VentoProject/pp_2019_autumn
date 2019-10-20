30d53440fb23a378b61ee48e913000ef8bf2c81b
VentoProject <vento.an@yandex.ru>
2019-10-21 02:35:33 +0300

    fix_test

---
+++ modules/task_1/khruleva_a_rows_max/rows_max_mpi.h
@@ -0,0 +1,14 @@
+// Copyright 2019 Khruleva Anastasia
+#ifndef MODULES_TASK_1_KHRULEVA_A_ROWS_MAX_H_
+#define MODULES_TASK_1_KHRULEVA_A_ROWS_MAX_H_
+
+#include <vector>
+#include <string>
+
+std::vector<int> getMatr(int rows, int cols);
+std::vector<int> getMaxMatr(const std::vector<int>& matr, int rows, int cols);
+std::vector<int> getParallelMatr(const std::vector<int>& matr, int rows, int cols);
+
+#endif  // MODULES_TASK_1_KHRULEVA_A_ROWS_MAX_H_
+
+
