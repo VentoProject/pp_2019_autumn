5e0c48b889a0e7074585bc451fb34047c4fab650
VentoProject <vento.an@yandex.ru>
2019-12-19 23:28:16 +0300

    new_task3

---
+++ modules/task_3/khruleva_multI_matr_ccs/multi_css_m.h
@@ -0,0 +1,21 @@
+#include <mpi.h>
+#include <vector>
+#ifndef MODULES_TASK_3_KHRULEVA_A_MULTIPLICATION_SPARSE_MATRIX_MULT_SPARSE_MATRIX_MULT_H_
+#define MODULES_TASK_3_KHRULEVA_A_MULTIPLICATION_SPARSE_MATRIX_MULT_SPARSE_MATRIX_MULT_H_
+struct MatrixCCS {
+  std::vector<double> val;
+  std::vector<int> JA;
+  std::vector<int> IA;
+  int Col, Rows, xxx;
+
+  friend const std::vector<double> operator*(const MatrixCCS& X,
+    const MatrixCCS& Y);
+};
+
+MatrixCCS CCS(const std::vector<double> Mart_N,
+  const int Col_N, const int Rows_N);
+
+std::vector<double> multiplication(MatrixCCS X, MatrixCCS Y);
+std::vector<double> randomMatrix(const int col, const int rows);
+
+#endif  // MODULES_TASK_3_KHRULEVA_A_MULTIPLICATION_SPARSE_MATRIX_MULT_SPARSE_MATRIX_MULT_H_
